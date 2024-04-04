75d4b95c595bd635f8f9e4954088b2aabae7b658
Suriya Vijay <rsathiyaprakash10@gmail.com>
2024-04-04 21:59:59 +0530

    Add authentication api in backend

--- backend/index.js
+++ backend/index.js
@@ -4,13 +4,29 @@ require("./db");
 const express = require("express");
 const cors = require("cors");
 
+// controller
+const {signUp, signIn} = require("./controllers/authController");
 
 const app = express();
 
 // middleware configurations
-app.use(cors())
+app.use(cors());
 app.use(express.json());
 
+// routers
+app.post("/signup", signUp);
+app.post("/signin", signIn);
+
+// error handling configurations
+app.use((err, req, res, next) => {
+    err.statusCode = err.statusCode || 500;
+    err.status = err.status || "error";
+
+    res.status(err.statusCode).json({
+        status: err.status,
+        message: err.message
+    });
+});
 
 // server configuration
 app.listen(process.env.PORT, ()=>{
